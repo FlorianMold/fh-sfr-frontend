196a0269757e34f87f4cddefc479d0f8a3d071de
Florian Mold <florian.mold@agnet.at>
2022-04-03 14:16:22 +0200

    feat(libs/web-components): add prototype web-component

--- libs/web-components/src/lib/web-components.ts
+++ libs/web-components/src/lib/web-components.ts
@@ -1,3 +1,19 @@
-export function webComponents(): string {
-  return 'web-components';
+export class AutoCompleteWebComponent extends HTMLElement {
+  public static observedAttributes = ['title'];
+
+  connectedCallback() {
+    console.log('Appended and connected to document');
+  }
+
+  disconnectedCallback() {
+    console.log('Disconnected from document');
+  }
+
+  attributeChangedCallback(name: string, old: string, value: string) {
+    console.log(`Element's attribute ${name} was ${old} and is now ${value}`);
+    this.innerHTML = `<h1>Welcome From ${this.title}!</h1>`;
+  }
+  
 }
+
+customElements.define('ui-autocomplete-web-component', AutoCompleteWebComponent);
